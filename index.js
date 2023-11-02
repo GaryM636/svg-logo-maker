10ac955dfe7799e3069a11ff51f64f8a67036612
GaryM636 <142124124+GaryM636@users.noreply.github.com>
2023-11-01 17:44:54 -0700

    added jest tests, corresponding js files pass tests, running program saves files to assets folder

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Square, Triangle} = require('./lib/shapes')
+const validateColor = require('./lib/validateColor');
+const { Circle, Square, Triangle } = require('./lib/shapes')
+
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'textChar',
+      message: 'Please enter the text you would like in your logo (up to 3 characters):',
+      validate: function (input) {
+        if (input.length <= 3) {
+          return true;
+        } else {
+          return 'Please enter up to 3 characters.';
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Please enter the text color for your logo (CSS color name or hex code):',
+      validate: validateColor,
+    },
+    {
+      type: 'list',
+      name: 'logoShape',
+      message: 'Please select the logo shape:',
+      choices: ['circle', 'square', 'triangle'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Please enter the shape color for your logo (CSS color name or hex code):',
+      validate: validateColor,
+    },
+  ])
+  .then((answers) => {
+    // Determine the selected shape and create the corresponding object
+    let shape;
+    if (answers.logoShape === 'circle') {
+        shape = new Circle(answers.color, answers.textChar, answers.textColor);
+    } else if (answers.logoShape === 'square') {
+        shape = new Square(answers.color, answers.textChar, answers.textColor);
+    } else if (answers.logoShape === 'triangle') {
+        shape = new Triangle(answers.color, answers.textChar, answers.textColor);
+    } else {
+        console.log('Invalid shape selected.');
+        return;
+    }
+
+    // Generate the SVG markup for the selected shape
+    const svgMarkup = shape.render();
+
+    const savedAs = `./Assets/${answers.logoShape}-logo.svg`;
+
+    // Save the SVG markup to a file with the formatted logo name in the Assets folder
+    fs.writeFileSync(savedAs, svgMarkup, 'utf-8');
+
+    console.log(`Logo created and saved as "${savedAs}".`);
+});
+
+
+
+
+
+
 
---
+++ tests/shapes.test.js
@@ -0,0 +1,36 @@
+const { Circle, Square, Triangle } = require('../lib/shapes');
+
+describe('Shapes', () => {
+  it('should create a circle with specified text and colors', () => {
+    const circle = new Circle('red', 'A', 'blue');
+    const expectedSvg = `<svg width="300" height="200">
+        <circle cx="150" cy="100" r="100" fill="red" />
+        <text x="50%" y="50%" text-anchor="middle" fill="blue" font-size="100px" dy=".3em">A</text>
+    </svg>`;
+
+    const result = circle.render();
+    expect(result).toEqual(expectedSvg);
+  });
+
+  it('should create a square with specified text and colors', () => {
+    const square = new Square('green', 'B', 'yellow');
+    const expectedSvg = `<svg width="300" height="200">
+        <rect width="200" height="200" fill="green" />
+        <text x="33%" y="66%" text-anchor="middle" fill="yellow" font-size="100px" dy=".3em">B</text>
+      </svg>`;
+
+    const result = square.render();
+    expect(result).toEqual(expectedSvg);
+  });
+
+  it('should create a triangle with specified text and colors', () => {
+    const triangle = new Triangle('orange', 'C', 'purple');
+    const expectedSvg = `<svg height="300" width="200">
+        <polygon points="100,10 25,290 175,290" fill="orange" />
+        <text x="50%" y="70%" text-anchor="middle" fill="purple" font-size="100px" dy=".3em">C</text>
+      </svg>`;
+
+    const result = triangle.render();
+    expect(result).toEqual(expectedSvg);
+  });
+});
